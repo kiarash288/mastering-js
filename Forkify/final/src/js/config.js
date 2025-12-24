6f0b45b90ddc9c563b8c6c4a7ce8c1750c525146
kiarash288 <rabieekiarash7@gmail.com>
2025-12-23 23:22:16 -0800

    Forkify(models, helpers)

---
+++ Forkify/final/src/js/config.js
@@ -0,0 +1,7 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '<YOUR_KEY>';
+export const MODAL_CLOSE_SEC = 2.5;
+
+// زمانی که میخوایم یک متغیر ثابت تعریف کنیم کل حروفش رو بزرگ میکنیم و وقتی میخوایم ازش تو یک فایل دیگه استفاده کنیم باید کنارش export بنویسم که بعدا بتونم import اش بکنیم 
--- mapty/1.py
+++ mapty/1.py
@@ -1,21 +1,20 @@
-user_info = {
-    "name": "Ali",
-    "family": "Alavi",
-    "age": 25
+# دیکشنری اصلی که مقادیر آن خودشان دیکشنری هستند
+garage = {
+    "car1": {"brand": "BMW", "year": 2022, "color": "Black"},
+    "car2": {"brand": "Tesla", "year": 2023, "color": "White"},
+    "car3": {"brand": "Toyota", "year": 2021, "color": "Blue"}
 }
 
 
-required_keys = ["income", "friends", "father_name", "mother_name"]
-
-for key in required_keys:
-    if key not in user_info:
-        print(f"فیلد '{key}' در دیکشنری تعریف نشده است")
+for key in garage.keys()
 
 
-# ------------------------------------------------------------------------------------------------
 
-if user_info.get("income") is None:
-    print("میزان درآمد تعریف نشده است")
 
-if user_info.get("friends") is None:
-    print("اسم دوستان تعریف نشده است")
+
+    inner_dict = garage[key]
+    
+    print(f"--- Information for {key} ---")
+    print(f"Full Value: {inner_dict}") 
+    
+    print(f"Brand: {inner_dict['brand']}")
